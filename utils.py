dfe00d607989e296bb7473f7ebfaefc3fed747e2
Joshua Wharton <joshuawharton1997@gmail.com>
2020-06-07 22:16:03 +0100

    Add files via upload

--- utils.py
+++ utils.py
@@ -7,6 +7,25 @@ from tornado.concurrent import run_on_executor
 
 class Utils(object):
    @staticmethod
+   def line_prepender(filename, line):
+      file_dir = '/'.join(filename.split('/')[:-1])
+      if not os.path.isdir(file_dir):
+         os.mkdir(file_dir)
+      if not os.path.isfile(filename):
+         with open(filename, 'w') as f:
+            pass
+
+      with open(filename, 'r+') as f:
+         content = f.read()
+
+         if not content and '-- starting hand' not in line:
+            # Only want to start writing to file at beginning of hand
+            return
+
+         f.seek(0, 0)
+         f.write(line.rstrip('\r\n') + '\n' + content)
+
+   @staticmethod
    def write_file(filename, contents, check_directory=True):
       file_dir = '/'.join(filename.split('/')[:-1])
       if check_directory and not os.path.isdir(file_dir):
